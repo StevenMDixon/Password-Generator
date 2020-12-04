7d3b323a2217d133843526e9e5e3137651215b66
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2020-12-04 10:05:11 -0500

    webpack setup for basic site

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -11,14 +12,33 @@ module.exports = {
       exclude: /node_modules/,
       use: ['babel-loader'],
     },
+    {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+    },
+
   ]
   },
+  plugins: [
+    // Re-generate index.html with injected script tag.
+    // The injected script tag contains a src value of the
+    // filename output defined above.
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src", "index.html")
+    })
+    // new CopyPlugin({
+    //     patterns: [
+    //       { from: "source", to: "dest" },
+    //       //{ from: "other", to: "public" },
+    //     ],
+    //   }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    port: 9000
+    contentBase: path.join(__dirname, 'src'),
+    port: 3000
   }
 };
