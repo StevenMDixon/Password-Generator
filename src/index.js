5b603d628362a15d5af9f2c116b51a90c93790bf
StevenDixonDev <34195467+StevenDixonDev@users.noreply.github.com>
2020-12-18 23:09:20 -0500

    fleshing out front end

--- src/index.js
+++ src/index.js
@@ -7,9 +7,13 @@ import generateRandomCharacter from './randomCharacter';
     Store criteria in variables
     generate password
 */
-
-let charactersNeeded = ['alphaN', 'alphaA', 'numeric'];
-let passwordLength = 10;
+// store options here
+const options = {
+    uppercase: false,
+    lowercase: false,
+    numeric: false,
+    special: false
+};
 
 // function used to generate password using a for loop
 function generatePassWord(characters, lengthofPassword){
@@ -20,9 +24,44 @@ function generatePassWord(characters, lengthofPassword){
     return password;
 };
 
+// set password in the DOM
+function assignPassword(password){
+    document.getElementById('password-output').textContent = password;
+}
 
+//get list of character types for random generator
+function getCharactersNeed(options){
+    const charactersNeeded = [];
+    for(let [v, k] of Object.entries(options)){
+        if(k) charactersNeeded.push(v) 
+    }
+    return charactersNeeded;
+}
 
-// add an event listener to the page to listen for a click on the password generate button
-document.getElementById('password-generator-button').addEventListener('click', ()=>{
-    document.getElementById('password-output').textContent = generatePassWord(charactersNeeded, passwordLength);
+function handleGeneratePasswordClick(e){
+    let PasswordLength =  document.getElementById("passWordLength").value
+    let password = generatePassWord(getCharactersNeed(options), PasswordLength)
+    assignPassword(password);
+}
+
+function handleOptionClick(e){
+    if(this.dataset.toggleType){
+        options[this.dataset.toggleType] = this.checked
+
+    }else {
+        // handle error
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // add an event listener to the page to listen for a click on the password generate button
+    document.getElementById('password-generator-button').addEventListener('click', handleGeneratePasswordClick);
+
+    const optionButtons = document.getElementsByClassName('option');
+
+    for(let i = 0; i < optionButtons.length; i++){
+        optionButtons[i].addEventListener('click', handleOptionClick)
+    }
+
 })
--- src/randomCharacter.js
+++ src/randomCharacter.js
@@ -1,12 +1,10 @@
-const characterRanges = {
-    alphaA: [65, 90],
-    alphaN: [97, 122],
+export const characterRanges = {
+    uppercase: [65, 90],
+    lowercase: [97, 122],
     numeric: [48, 57],
-    special1: [33, 47],
-    special2: [58, 64],
-    special3: [92, 95],
-    special4: [123, 126]
+    special: [[33, 47],[58, 64], [92, 95], [123, 126]]
 }
+
 
 export function randomNumber(startofRange, endofRange){
     return (Math.floor(Math.random() * (endofRange) + startofRange))
@@ -17,8 +15,18 @@ export function generateRandomNumber(arrayofItems){
     return arrayofItems[randomIndex];
 }
 
-export function createRange([start, end]){
-    return Array.from(new Array((end - start) + 1), (x, i) => start + i);
+export function createRange(ranges){
+    if(Array.isArray(ranges[0])) {
+        return ranges.reduce((acc, range)=> {
+            let [start, end] = range;
+            acc = [...acc, ...Array.from(new Array((end - start) + 1), (x, i) => start + i)];
+            return acc;
+        }, []);
+    } else {
+        let [start, end] = ranges;
+        return Array.from(new Array((end - start) + 1), (x, i) => start + i);
+    }
+    
 }
 
 export function generateRandomCharacter(types){
